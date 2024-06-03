67b1548cb2e075282fac429f9d0793d1ddee87e3
vishalBelwal <vishalbelwal52@gmail.com>
2024-06-03 20:45:30 +0530

    login in and signup complete

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBTN from "./header/LogoutBTN";
+import Button from './Button';
+import Input from './Input';
 
 export {
   Header,
   Footer,
   Container,
   Logo,
-  LogoutBTN
+  LogoutBTN,
+  Button,
+  Input
 }
