5edfba5cc7085a28e6c205d54470561e8759066b
vishalBelwal <vishalbelwal52@gmail.com>
2024-06-04 19:02:18 +0530

    authorisation layout

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,30 @@
+//mechanism to protect pages and routes
+
+import React,{useState, useEffect} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({children, authentication = true}){
+
+  const navigate = useNavigate()
+  const [loader, setLoader] = useState(true)
+  const authStatus = useSelector(state => state.auth.status)
+
+  useEffect(() => {
+
+    // if(authStatus === true){
+    //   navigate("/")
+    // } else if(authStatus === false){
+    //   navigate("/login")
+    // }
+
+    if (authentication && authStatus !== authentication) {
+      navigate("/login")
+    } else if(!authentication && authStatus !== authentication){
+      navigate("/")
+    }
+    setLoader(false)
+  }, [authStatus, navigate, authentication])  //agar in sab me kuch bhi change ho to useEffect ko fir se run karo
+
+  return loader ? <h1>Loading......</h1> : <>{children}</>;
+}
