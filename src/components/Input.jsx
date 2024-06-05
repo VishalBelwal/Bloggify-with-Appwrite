5175e2cd0fbd9a0306a00bf2f8c5759028a629ab
vishalBelwal <vishalbelwal52@gmail.com>
2024-06-05 19:10:06 +0530

    onclick fix

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -9,14 +9,14 @@ const Input = React.forwardRef( function Input({
   return (
     <div className='w-full'>
       {
-        label && <label className='inline-block mb-1 pl-1' htmlFor={props.id}>
+        label && <label className='inline-block mb-1 pl-1' htmlFor={id}>
           {label}
         </label>
       }
 
       <input type={type} className={` px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className} `} ref={ref} {...props} id={id}/>
 
-      //useRef se do different and individual components ke beech me refrence pass bhi kiya jata hai and state ki form me liye bhi jaata hai
+      {/* //useRef se do different and individual components ke beech me refrence pass bhi kiya jata hai and state ki form me liye bhi jaata hai */}
     </div>
   )
 })
